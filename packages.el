3bc0c34a43b453d99a6b1c0af7bbfe9fd384a28f
Brian McGillion <bmg.avoin@gmail.com>
2023-03-01 14:44:29 +0400

    Cleanup config
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- packages.el
+++ packages.el
@@ -78,14 +78,5 @@
 ;; Get the latest completion
 ;;(unpin! helm)
 
-;; Code related
-;;(package! x86-lookup)
-;;(package! nasm-mode)
-
-
 (package! org-reveal)
 (package! ox-reveal)
-;;(package! htmlize)
-
-(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
-(package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
