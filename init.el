17123cb53d3937b817e898ad68adc0d8b3c41713
Brian McGillion <bmg.avoin@gmail.com>
2023-11-19 18:57:50 +0400

    Disable: unised cl and haskell
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- init.el
+++ init.el
@@ -117,7 +117,7 @@
        ;;beancount         ; mind the GAAP
        (cc +lsp +tree-sitter)               ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
-       common-lisp       ; if you've seen one lisp, you've seen them all
+       ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
@@ -137,7 +137,7 @@
        ;;gdscript          ; the language you waited for
        ;;(go +lsp +tree-sitter)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
-       (haskell +lsp +treesitter)  ; a language that's lazier than I am
+       ;;(haskell +lsp +treesitter)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        ;;json              ; At least it ain't XML
