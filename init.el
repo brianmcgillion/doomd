e998de19f5fdfa63226c2fcb7d3e3d7639134141
Brian McGillion <bmg.avoin@gmail.com>
2024-09-02 19:41:59 +0400

    add dabbrev
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
 
        :completion
        ;;(company +childframe) ; the ultimate code completion backend
-       (corfu +orderless +icons)  ; complete with cap(f), cape and a flying feather!
+       (corfu +orderless +icons +dabbrev)  ; complete with cap(f), cape and a flying feather!
        (helm +fuzzy +icons)  ; the *other* search engine for love and life
        ;;ido                 ; the other *other* search engine...
        ;;ivy                 ; a search engine for love and life
