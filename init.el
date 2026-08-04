2e62eef61b4499467626005d9de8e1476e2689f7
Brian McGillion <bmg.avoin@gmail.com>
2026-08-04 09:16:41 +0400

    ghostel: replace vterm with ghostel
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- init.el
+++ init.el
@@ -83,7 +83,8 @@
        ;;eshell              ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term                ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       ;;vterm             ; almost the best terminal emulation in Emacs
+       (ghostel +everywhere) ; the best terminal emulation in Emacs
 
        :checkers
        (syntax +childframe +icons); tasing you for every semicolon you forget
